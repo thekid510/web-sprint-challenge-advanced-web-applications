735cf1031c0ed94b0a4de6dadebfd34447f3974d
thekid510 <76237067+thekid510@users.noreply.github.com>
2021-06-18 14:41:26 -0700

    built out login and private routes

--- src/App.js
+++ src/App.js
@@ -1,8 +1,19 @@
 import React, { useState } from "react";
-import { BrowserRouter as Router, Route } from "react-router-dom";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 
 import Login from "./components/Login";
 import "./styles.scss";
+import PrivateRoute from '../src/components/PrivateRoute';
+import BubblePage from "./components/BubblePage";
+
+const handleLogOut= ()=> {
+
+
+
+
+
+}
+
 
 function App() {
   return (
@@ -12,8 +23,12 @@ function App() {
           Color Picker Sprint Challenge
           <a data-testid="logoutButton" href="#">logout</a>
         </header> 
-
+        <Switch>
+        <PrivateRoute path ="/bubbles">
+          <BubblePage />
+        </PrivateRoute>
         <Route exact path="/" component={Login} />
+        </Switch>
       </div>
     </Router>
   );
--- src/components/Login.js
+++ src/components/Login.js
@@ -1,11 +1,13 @@
 import axios from "axios";
 import React, { useEffect, useState } from "react";
+import { useHistory} from 'react-router'
 
-const initialValues= {username:"Lambda", password:"School" };
+
+const initialValues= {username:"", password:"" };
 
 const Login = () => {
   const [formValues, setFormValues] = useState(initialValues);
-
+  const { push } = useHistory();
   const handleChange = (e) => {
 
     setFormValues({...formValues, [e.target.name]: e.target.value})
@@ -13,7 +15,16 @@ const Login = () => {
 
   const handleSubmit = (e) => {
     e.preventDefault();
-    axios.post('http://localhost:5000/api/login',initialValues)
+    axios.post('http://localhost:5000/api/login',formValues)
+    .then(res => {
+      // console.log(res)
+      localStorage.setItem("token", res.data.payload);
+      push("/bubbles")
+
+    })
+    .catch(err => {
+      console.log(err)
+    })
   }
   // make a post request to retrieve a token from the api
   // when you have handled the token, navigate to the BubblePage route
@@ -28,14 +39,14 @@ const Login = () => {
       <div data-testid="loginForm" className="login-form">
        <form onSubmit={handleSubmit}>
          <label htmlFor="username">Username </label>
-        <input 
+        <input data-testid="username"
         id="username"
         name="username"
         value={formValues.username}
         onChange={handleChange}
         />
         <label htmlFor="password">Password</label>
-        <input 
+        <input data-testid="password"
          id="password"
          name="password"
          value={formValues.password}
